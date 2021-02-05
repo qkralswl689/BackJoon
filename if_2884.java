ec742cc177afc2f10dfaaaa209f23d21b1621930
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-05 18:18:28 +0900

    02.05

---
+++ if_2884.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class if_2884 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if ((0 <= h || h <= 23) || (m>= 0 || m <= 59)) {
+			if(true) {
+				
+			}
+		}
+
+	}
+
+}
